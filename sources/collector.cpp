5d3ed506c99d6af2b435faa13bb6c30c07c68fa6
nmsMentor <43293575+nmsMentor@users.noreply.github.com>
2018-09-16 00:36:17 -0400

    Update collector.cpp

--- sources/collector.cpp
+++ sources/collector.cpp
@@ -1,1 +1,10 @@
-
+/**
+ * collector.cpp - some program to control ball collector mechanism
+ *
+ * written for Northern Middle School Robotics
+ * 2018-19 VRC "Turning Point"
+ *
+ * 
+ * @TODO: write program
+ * @TODO:
+ */
