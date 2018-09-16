f6126ad1ecc6bc2bb744f8adde2a352c1152b7ff
nmsMentor <43293575+nmsMentor@users.noreply.github.com>
2018-09-16 00:45:25 -0400

    Create skills.cpp

---
+++ routines/skills.cpp
@@ -0,0 +1,20 @@
+/**
+ * skills.c - Holds programming skills routines (60 second)
+ * borrowed from Brandon
+ *
+ * @TODO: convert from *.c to *.cpp
+ */
+
+
+void skillsFoward() {
+    
+}
+
+string skillsRoutines[] = { "Forward" };
+void goSkills(int routine) {
+    switch(routine) {
+        default: case 0:
+            skillsFoward();
+            break;
+    }
+}
