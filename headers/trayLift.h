fccbb16b29063c6a76cddc1b7117b4e514618314
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-07 13:59:29 -0500

    tray lift header file
    header file for thread to raise and lower tray lift

---
+++ headers/trayLift.h
@@ -0,0 +1,63 @@
+#include "vex.h"
+
+// traylift speed variable
+int trayLiftSpeedPCT = 100;       
+
+/** 	--- Tray Movement Shortend Function Call Setup ---
+   *  tray lift up:     	cmd runs tray lift up manually by pressing of Button R1 (Manual Operation)
+   *  tray lift down:  	  cmd runs tray lift down manually by pressing of Button R2 (Manual Operation)
+   *  tray lift reset:  	cmd runs tray lift down from BUTTONDOWN press till limit switch is triggered (Automatic Reset)
+   */
+void trayLiftSpinUp(){
+  TrayLiftmotor.spin( reverse, trayLiftSpeedPCT, velocityUnits::pct );
+}
+
+void trayLiftSpinDown(){
+  TrayLiftmotor.spin( forward, trayLiftSpeedPCT, velocityUnits::pct );
+}
+
+///////////////////////////////////////////////////
+//
+// --- Tray Lift Thread ---
+//
+///////////////////////////////////////////////////
+
+// declare state variable for tray lift
+bool manual_mode = true;          
+
+int TLSelection_Thread(){
+  
+  int count = 0;
+  while(true){
+
+  // Manual: Tray Up by ButtonR1
+  if((( manual_mode == true ) && (( con.ButtonR1.pressing()) == true )))
+  manual_mode = true;
+  while(( manual_mode == true ) && (( con.ButtonR1.pressing() == true))){
+    trayLiftSpinUp();}
+    TrayLiftmotor.stop( brakeType::brake );
+    manual_mode = true;
+  
+  // Manual: Tray Down by ButtonR2 with Hard Stop by Limit Switch   
+  if(( manual_mode == true ) && ((con.ButtonR2.pressing()) == true ))
+    manual_mode = true;
+    while(( manual_mode == true ) && ( con.ButtonR2.pressing() == true ) && 
+    ( TrayLiftlimit.pressing() == false )){ trayLiftSpinDown(); }
+    TrayLiftmotor.stop( brakeType::brake );
+    manual_mode = true;
+  
+  // Automatic: Tray Down Return by ButtonDown Press with Hard Stop by Limit Switch
+  if((con.ButtonDown.pressing()) == true )
+    manual_mode = false;
+    while(( manual_mode == false ) && (( TrayLiftlimit.pressing()) == false )){
+    trayLiftSpinDown(); }
+    TrayLiftmotor.stop( brakeType::brake );
+    manual_mode = true;
+	
+	// Allow other tasks to run
+  // don't hog the cpu :)
+  vex::this_thread::sleep_for( 25 );
+
+  }
+  return(0);
+}
