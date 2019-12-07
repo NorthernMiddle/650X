6af2de7fe7b6a666e4117261c6f1709692743ad9
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-07 14:06:05 -0500

    Create vex.h

---
+++ headers/vex.h
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\reece.turner                                     */
+/*    Created:      Fri Dec 06 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// LFmotor              motor         1               
+// LBmotor              motor         2               
+// RFmotor              motor         3               
+// RBmotor              motor         4               
+// TLmotor              motor         8               
+// TLlimit              limit         B               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
