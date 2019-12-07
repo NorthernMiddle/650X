1443cdfb7e7705d1afbd08db6776daa69f37a3cc
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-07 14:24:55 -0500

    Update auton.cpp

--- routines/auton.cpp
+++ routines/auton.cpp
@@ -1,27 +1,106 @@
-/**
- * auton.c - Holds autonomous routines (15 second)
- * borrowed from Brandon
- *
- * @TODO: convert from *.c to *.cpp
- */
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       auton.cpp                                                 */
+/*    Author:       NMS_RP                                                    */
+/*    Created:      17 Dec 2019                                               */
+/*    Description:  15-Second Autonomous Routines                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
-void autonMogo() {
+//------------------------------------------------------------------------------
+//
+// Red Alliance Autonomous Routines
+//
+//------------------------------------------------------------------------------
 
+void Auton_Red01() {
+  // enter Red Auton #1 code here...
 }
 
-void autonBlock() {
-    
+void Auton_Red02() {
+  // enter Red Auton #2 code here...
+}
+
+void Auton_Red03() {
+  // enter Red Auton #3 code here...
+}
+
+void Auton_Red04() {
+  // enter Red Auton #4 code here...
 }
 
 
-string autonRoutines[] = { "Mogo", "Block" };
-void goAuton(int routine) {
-    switch(routine) {
+//------------------------------------------------------------------------------
+//
+// Blue Alliance Autonomous Routines
+//
+//------------------------------------------------------------------------------
+
+void Auton_Blue01() {
+  // enter Blue Auton #1 code here...
+}
+
+void Auton_Blue02() {
+  // enter Blue Auton #2 code here...
+}
+
+void Auton_Blue03() {
+  // enter Blue Auton #3 code here...
+}
+
+void Auton_Blue04() {
+  // enter Blue Auton #4 code here...
+}
+
+
+//------------------------------------------------------------------------------
+//
+// Alliance Autonomous Routines Selector
+//
+//------------------------------------------------------------------------------
+
+std::string autonRoutines[] = { "Red01", "Red02", "Red03", "Red04", "Blue01", "Blue02", "Blue03", "Blue04" };
+void goAuton(int autonomousSelection) {
+    switch(autonomousSelection) {
         default: case 0:
-            autonMogo();
+            // Auton Red01 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Red01 Selected ");
+            Auton_Red01();
             break;
         case 1:
-            autonBlock();
+            // Auton Red02 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Red02 Selected ");
+            Auton_Red02();
+            break;
+        case 2:
+            // Auton Red03 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Red03 Selected ");
+            Auton_Red03();
+            break;
+        case 3:
+            // Auton Red04 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Red04 Selected ");
+            Auton_Red04();
+            break;
+        case 4:
+            // Auton Blue01 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Blue01 Selected ");
+            Auton_Blue01();
+            break;
+        case 5:
+            // Auton Blue02 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Blue02 Selected ");
+            Auton_Blue02();
+            break;
+        case 6:
+            // Auton Blue03 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Blue03 Selected ");
+            Auton_Blue03();
+            break;
+        case 7:
+            // Auton Blue04 Selected
+            Brain.Screen.printAt( 60, 125, "Auton Blue04 Selected ");
+            Auton_Blue04();
             break;
     }
 }
