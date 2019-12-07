ab26f022d0380281509059de297bd5d2da9807a5
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-07 14:02:58 -0500

    robot-config.h
    Change to VEXcode Text formatted text.
    Change to C++ code

--- headers/robot-config.h
+++ headers/robot-config.h
@@ -1,32 +1,18 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor LFmotor;
+extern motor LBmotor;
+extern motor RFmotor;
+extern motor RBmotor;
+extern motor TLmotor;
+extern limit TLlimit;
+
 /**
- * robot-config.h - header file for VRC V5
- * written for Northern Middle School Robotics Team 650X
- * VRC 2018-19 "Turning Point"
- *
- * @TODO: add setup code for collector mechanism
- * @TODO: add setup code for shooter mechansim
- * @TODO: add setup code for V5 vision sensor
- * @TODO: 
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
  */
- 
- using namespace vex;
- vex::brain Brain;
- 
-//drive motors
-vex::motor leftMotor (vex::PORT1, vex::gearSetting::ratios18_1, false);
-vex::motor rightMotor (vex::PORT10,  vex::gearSetting::ratio18_1, true);
-
-//claw motor
-vex::motor clawMotor (vex::PORT3, vex::gearSetting::ratio18_1,true);
-
-//arm motor
-vex::motor armMotor (vex::PORT8, vex::gearSetting::ratio18_1,true);
-
-//collector mechanism
-????
- 
-//shooter mechanism
-????
- 
-//V5 vision sensor
-????
+void  vexcodeInit( void );
