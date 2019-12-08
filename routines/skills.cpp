78ac1b065c776648261d2d14c8575f3e7266f911
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-08 09:23:50 -0500

    Update skills.cpp

--- routines/skills.cpp
+++ routines/skills.cpp
@@ -1,20 +1,48 @@
-/**
- * skills.c - Holds programming skills routines (60 second)
- * borrowed from Brandon
- *
- * @TODO: convert from *.c to *.cpp
- */
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       skills.h                                                  */
+/*    Author:       NMS_RP                                                    */
+/*    Created:      23 Nov 2019                                               */
+/*    Description:  60 Second Skills Routines                                 */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 
-void skillsFoward() {
-    
+
+/**	skillsRed.h
+  *	
+  *
+  */
+void skillsRed() {
+  // enter your Red Alliance skills program here...
 }
 
-string skillsRoutines[] = { "Forward" };
+/**	skillsBlue.h
+  *	
+  *
+  */
+void skillsBlue() {
+  // enter your Blue Alliance skills program here...
+}
+
+//////////////////////////////////////
+//
+//	--- Skills Routine Selector ---
+//
+//////////////////////////////////////
+
+std::string skillsRoutines[] = { "Red, Blue" };
 void goSkills(int routine) {
     switch(routine) {
         default: case 0:
-            skillsFoward();
+			// Red Skills Routine Selected
+			Brain.Screen.printAt( 60, 125, "Red Skills Routine Selected ");
+            skillsRed();
+            break;
+		case 1:
+			// Blue Skills Routine Selected
+			Brain.Screen.printAt( 60, 125, "Blue Skills Routine Selected ");
+            skillsBlue();
             break;
     }
 }
