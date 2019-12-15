9b319913b7081b18832d4f8dccc18197bb13c3f7
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-15 01:17:38 -0500

    Update drive.h

--- headers/drive.h
+++ headers/drive.h
@@ -1,62 +1,104 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       drive.h                                                   */
-/*    Author:       NMS_RP                                                    */
-/*    Created:      03 Dec 2019                                               */
-/*    Description:  tank and arcade drive (left & right joystick options)     */
-/*                  for VEX V5 with deadband                                  */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+#ifndef DRIVE_H
+#define DRIVE_H
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]   [Description]       [Type]        [Port(s)]  [Reversed]
-// LBmotor  Left Back Motor     Green Motor    2         True     
-// LFmotor  Left Front Motor    Green Motor    1         True     
-// RBmotor  Right Back Motor    Green Motor    10        False      
-// RFmotor  Right Front Motor   Green Motor    9         False                
-// ---- END VEXCODE CONFIGURED DEVICES ----
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*   	Module:       drive.h                                                 */
+/*   	Author:       NMS_RP                                                  */
+/*   	Created:      14 Dec 2019                                             */
+/*  	Description:  tank and arcade drive (left & right joystick options)   */
+/*                     with deadband and cubic power input for VEX V5         */
+/*                                                                            */
+/* 		Robot Configuration:												  */	
+/* 		[Name]		[Description]			[Type]        	[Port(s)]		  */
+/* 		LBmotor  	Left  Rear  Motor       Green Motor   	2             	  */ 
+/* 		LFmotor  	Left  Front Motor       Green Motor   	1              	  */
+/* 		RBmotor  	Right Rear  Motor       Green Motor   	10                */
+/* 		RFmotor  	Right Front Motor       Green Motor   	9                 */
+/*																			  */
+/*----------------------------------------------------------------------------*/
 
 // VEX V5 C++ Project
 #include "vex.h"
 using namespace vex;
 
-// #region config_globals
-controller Controller1 = controller( controllerType::primary );
+controller Controller1 = Controller(ControllerType::Primary);
+
+// Variable Declarations
+const int deadband;
+int fwdBck_pct_left, fwdBck_pct_right;		// tank drive vars
+double drive_lval, drive_rval;				// tank drive vars
+int forward_pct_leftjoy, turn_pct_leftjoy;	// arcade drive left joystick deadband vars
+double drive_l_leftjoy, drive_r_leftjoy;	// arcade drive left joystick drive vars	
+int	forward_pct_rightjoy, turn_pct_rightjoy	// arcade drive right joystick deadband vars
+double drive_l_rightjoy, drive_r_rightjoy;	// arcade drive right joystick drive vars
 
 // deadband threshold
-const int deadband = 15; 
+deadband = 15; 
 
-// power reduction factor -- for controlled driving
-float prf = 0.80; // 80% of MAX
+// tank drive --- variable intializations
+fwdBck_pct_left = Controller1.Axis3.position();
+fwdBck_pct_right = Controller1.Axis2.position();
+drive_lval = Controller1.Axis3.value();
+drive_rval = Controller1.Axis2.value();
 
-// tank drive variable declarations with intialization
-int fwdBck_pct_left = Controller1.Axis3.position();
-int fwdBck_pct_right = Controller1.Axis2.position();
-double drive_lval = Controller1.Axis3.value() *prf;
-double drive_rval = Controller1.Axis2.value() *prf;
+/** Cubic Scale Factor for Tank Drive Power
+  * Functions used to achieve more sensitivity at low motor power
+  * used for precise movements while still being able to use 100% power
+  */
+int cube_drive_lval(int drive_lval){				// tank drive cube scale factor
+    return  pow(drive_lval/100.0, 3.0)*100.0;
+}
+int cube_drive_rval(int drive_rval){				// tank drive cube scale factor
+    return  pow(drive_rval/100.0, 3.0)*100.0;
+}
 
-// arcade drive variable declarations with intialization
 
+/** arcade drive variable intializations
+  * left joystick variable intializations
+  * - deadband variables intializations
+  * - drive variables intializations
+  *
+  * right joystick variable intializations
+  * - deadband variables intializations
+  * - drive variables intializations
+  *
+  */
 /* --- LEFT JOYSTICK --- */
-int forward_pct_leftjoy = Controller1.Axis3.position();
-int turn_pct_leftjoy = Controller1.Axis4.position();
-double drive_l_leftjoy = (forward_pct_leftjoy + turn_pct_leftjoy) *prf;   // left '+'
-double drive_r_leftjoy = (forward_pct_leftjoy - turn_pct_leftjoy) *prf;   // right '-'
+forward_pct_leftjoy = Controller1.Axis3.position();
+turn_pct_leftjoy = Controller1.Axis4.position();
+drive_l_leftjoy = forward_pct_leftjoy + turn_pct_leftjoy;   // left '+'
+drive_r_leftjoy = forward_pct_leftjoy - turn_pct_leftjoy;   // right '-'
 
 /* --- RIGHT JOYSTICK --- */
-int forward_pct_rightjoy = Controller1.Axis2.position();
-int turn_pct_rightjoy = Controller1.Axis1.position();
-double drive_l_rightjoy = (forward_pct_rightjoy + turn_pct_rightjoy) *prf;   // left '+'
-double drive_r_rightjoy = (forward_pct_rightjoy - turn_pct_rightjoy) *prf;   // right '-'
+forward_pct_rightjoy = Controller1.Axis2.position();
+turn_pct_rightjoy = Controller1.Axis1.position();
+drive_l_rightjoy = forward_pct_rightjoy + turn_pct_rightjoy;   // left '+'
+drive_r_rightjoy = forward_pct_rightjoy - turn_pct_rightjoy;   // right '-'
 
 
-//////////////////////////////////////////////////////////
+/** Cubic Scale Factor for Arcade Drive Power
+  * Functions used to achieve more sensitivity at low motor power
+  * used for precise movements while still being able to use 100% power
+  */
+int cube_drive_l_leftjoy(int drive_l_leftjoy){				// arcade drive cube scale factor left joystick control
+    return  pow(drive_l_leftjoy/100.0, 3.0)*100.0;
+}
+int cube_drive_r_leftjoyl(int drive_r_leftjoy){				// arcade drive cube scale factor left joystick control
+    return  pow(drive_r_leftjoy/100.0, 3.0)*100.0;
+}
+int cube_drive_l_rightjoy(int drive_l_rightjoy){			// arcade drive cube scale factor right joystick control
+    return  pow(drive_l_rightjoy/100.0, 3.0)*100.0;
+}
+int cube_drive_r_rightjoyl(int drive_r_rightjoy){			// arcade drive cube scale factor right joystick control
+    return  pow(drive_r_rightjoy/100.0, 3.0)*100.0;
+}
+
+///////////////////////////////////////////////////////////////
 //
-//  --- Tank Drive Functions with Deadband ---
+//  --- Tank Drive with Deadband and Cubic Power Input ---
 //
-//////////////////////////////////////////////////////////
-
+///////////////////////////////////////////////////////////////
 /**
   * module: tank drive
   * @PARAM: Axis3 Left Side Motors
@@ -65,29 +107,26 @@ double drive_r_rightjoy = (forward_pct_rightjoy - turn_pct_rightjoy) *prf;   // 
   *
   */
 void tankDrive(){
-
   // deadband, set to 0 if below the deadband value
   if( abs( fwdBck_pct_left  ) < deadband ) fwdBck_pct_left  = 0;
   if( abs( fwdBck_pct_right ) < deadband ) fwdBck_pct_right = 0;
-
   // send to motors
   // left tank motors
-  LFmotor.spin( forward, drive_lval, velocityUnits::pct );
-  LBmotor.spin( forward, drive_lval, velocityUnits::pct );
+  LFmotor.spin( forward, cube_drive_lval, velocityUnits::pct );
+  LBmotor.spin( forward, cube_drive_lval, velocityUnits::pct );
   // right tank motors
-  RFmotor.spin( forward, drive_rval, velocityUnits::pct );
-  RBmotor.spin( forward, drive_rval, velocityUnits::pct );
-
+  RFmotor.spin( forward, cube_drive_rval, velocityUnits::pct );
+  RBmotor.spin( forward, cube_drive_rval, velocityUnits::pct );
+  
   // allow other tasks to run
   this_thread::sleep_for( 10 );
-
 }
 
-//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
 //
-//  --- Arcade Drive Functions with Deadband ---
+//  --- Arcade Drive with Deadband and Cubic Power Input ---
 //
-/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
 
 /**
   * module: ARCADE DRIVE: LEFT JOYSTICK
@@ -97,19 +136,18 @@ void tankDrive(){
   *
   */
 void arcadeDrive_leftJoy(){
-
   // DEADBAND, set to 0 if below the deadband threshold
   if( abs( forward_pct_leftjoy ) < deadband ) forward_pct_leftjoy = 0;
   if( abs( turn_pct_leftjoy    ) < deadband ) turn_pct_leftjoy = 0;
 
   // send to motors
-  // left arcade motors
-  LFmotor.spin( forward, drive_l_leftjoy, velocityUnits::pct );
-  LBmotor.spin( forward, drive_l_leftjoy, velocityUnits::pct );
-  // right arcade motors
-  RFmotor.spin( forward, drive_r_leftjoy, velocityUnits::pct );
-  RBmotor.spin( forward, drive_r_leftjoy, velocityUnits::pct );
-
+  // --- left arcade motors ---
+  LFmotor.spin( forward, cube_drive_l_leftjoy, velocityUnits::pct );
+  LBmotor.spin( forward, cube_drive_l_leftjoy, velocityUnits::pct );
+  // --- right arcade motors ---
+  RFmotor.spin( forward, cube_drive_r_leftjoy, velocityUnits::pct );
+  RBmotor.spin( forward, cube_drive_r_leftjoy, velocityUnits::pct );
+  
   // allow other tasks to run
   this_thread::sleep_for( 10 );
 }
@@ -123,19 +161,20 @@ void arcadeDrive_leftJoy(){
   *
   */
 void arcadeDrive_rightJoy(){
-
   // DEADBAND, set to 0 if below the deadband threshold
   if( abs( forward_pct_rightjoy ) < deadband ) forward_pct_rightjoy = 0;
   if( abs( turn_pct_rightjoy    ) < deadband ) turn_pct_rightjoy = 0;
 
   // send to motors
   // left arcade motors
-  LFmotor.spin( forward, drive_l_rightjoy, velocityUnits::pct );
-  LBmotor.spin( forward, drive_l_rightjoy, velocityUnits::pct );
+  LFmotor.spin( forward, cube_drive_l_rightjoy, velocityUnits::pct );
+  LBmotor.spin( forward, cube_drive_l_rightjoy, velocityUnits::pct );
   // right arcade motors
-  RFmotor.spin( forward, drive_r_rightjoy, velocityUnits::pct );
-  RBmotor.spin( forward, drive_r_rightjoy, velocityUnits::pct );
+  RFmotor.spin( forward, cube_drive_r_rightjoy, velocityUnits::pct );
+  RBmotor.spin( forward, cube_drive_r_rightjoy, velocityUnits::pct );
 
   // allow other tasks to run
   this_thread::sleep_for( 10 );
 }
+
+#endif
