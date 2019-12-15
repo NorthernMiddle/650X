1d0f22c24266a8ef13b8b17b8ddaf2230fec7c87
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-15 18:39:33 -0500

    Update threads.h

--- headers/threads.h
+++ headers/threads.h
@@ -3,7 +3,11 @@
   * @TODO: finish thread call for: drive, intake, and traylift
   */
 
+///////////////////////////////////////////////////////////////////
+//
 // --- thread call to drive ---
+//
+///////////////////////////////////////////////////////////////////
 int drive_t(){
   int driveCount = 0; // incrementor
   
@@ -16,6 +20,36 @@ int drive_t(){
  return( 0 );
 }
 
+///////////////////////////////////////////////////////////////////
+//
 // --- thread call to trayLift ---
+//
+///////////////////////////////////////////////////////////////////
+int trayLift_t(){
+ int trayLiftCount = 0; // incrementor
+ 
+ while(true){
+  Brain.Screen.printAt( 10, 60, "Task %d", trayLiftCount++);
+  
+  // don't hog the cpu :)
+   vex::this_thread::sleep_for( 25 );
+  }
+ return( 0 );
+}
 
+///////////////////////////////////////////////////////////////////
+//
 // --- thread call to intake ---
+//
+///////////////////////////////////////////////////////////////////
+int intake_t(){
+ int intakeCount = 0;
+ 
+ while(true){
+  Brain.Screen.printAt( 10, 60, "Task %d", intakeCount++);
+  
+   // don't hog the cpu :)
+   vex::this_thread::sleep_for( 25 );
+  }
+ return( 0 );
+}
