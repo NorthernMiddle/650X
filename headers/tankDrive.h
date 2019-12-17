30b5f87c2b2bfb4b8b519af1a4597fbb28411602
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-16 21:29:56 -0500

    Update and rename tankDrive.hpp to tankDrive.h

---
+++ headers/tankDrive.h
@@ -0,0 +1,17 @@
+#ifndef TANKDRIVE_H
+#define TANKDRIVE_H
+
+int cube_drive_lval(int drive_lval);    // tank drive cube scale factor
+int cube_drive_rval(int drive_rval);    // tank drive cube scale factor
+void deadBand_Tank();
+void tankDrive();
+
+int cube_drive_l_leftjoy(int drive_l_leftjoy);				  // arcade drive cube scale factor left joystick control
+int cube_drive_r_leftjoyl(int drive_r_leftjoy);				 // arcade drive cube scale factor left joystick control
+int cube_drive_l_rightjoy(int drive_l_rightjoy);			 // arcade drive cube scale factor right joystick control
+int cube_drive_r_rightjoyl(int drive_r_rightjoy);			// arcade drive cube scale factor right joystick control
+void deadBand_Arcade();
+void arcadeDrive_leftJoy();
+void arcadeDrive_rightJoy();
+
+#endif
