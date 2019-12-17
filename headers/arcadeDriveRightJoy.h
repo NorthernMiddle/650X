e6727cc70520d57cff8e9cd94e120fd08388b5f4
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-16 21:35:48 -0500

    Update arcadeDriveRightJoy.h

--- headers/arcadeDriveRightJoy.h
+++ headers/arcadeDriveRightJoy.h
@@ -1,4 +1,9 @@
 #ifndef ARCADEDRIVERIGHTJOY_H
 #define ARCADEDRIVERIGHTJOY_H
 
+int cube_drive_l_rightjoy(int drive_l_rightjoy);			 // arcade drive cube scale factor right joystick control
+int cube_drive_r_rightjoyl(int drive_r_rightjoy);			// arcade drive cube scale factor right joystick control
+void deadBand_Arcade();
+void arcadeDrive_rightJoy();
+
 #endif
