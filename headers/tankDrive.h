710363a7d5e895749a4cad3d1758b9a4375e92ef
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-16 21:35:27 -0500

    Update tankDrive.h

--- headers/tankDrive.h
+++ headers/tankDrive.h
@@ -7,10 +7,6 @@ void deadBand_Tank();
 void tankDrive();
 
 
-int cube_drive_l_rightjoy(int drive_l_rightjoy);			 // arcade drive cube scale factor right joystick control
-int cube_drive_r_rightjoyl(int drive_r_rightjoy);			// arcade drive cube scale factor right joystick control
-void deadBand_Arcade();
-void arcadeDrive_leftJoy();
-void arcadeDrive_rightJoy();
+
 
 #endif
