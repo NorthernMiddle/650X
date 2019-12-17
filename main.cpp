2ad51c560ade8dc6524f5cb493f999459c885b39
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-16 23:47:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -44,9 +44,9 @@ using namespace vex;
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+  // @TODO: Place 1:45sec timer on threads
+  // @TODO: At end of timer, end threads
   thread tankdrive_t = thread(tankDrive_f);     // thread DRIVE
   thread lift_t = thread(trayLift_f);           // thread TRAY LIFT
   thread spinners_t = thread(intake_f);         // thread INTAKE
-  
 }
