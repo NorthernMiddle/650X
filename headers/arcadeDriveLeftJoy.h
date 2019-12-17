a205980db6ada89c865fc88c7c87284f055cf988
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-16 21:32:52 -0500

    Update arcadeDriveLeftJoy.h

--- headers/arcadeDriveLeftJoy.h
+++ headers/arcadeDriveLeftJoy.h
@@ -1,5 +1,8 @@
 #ifndef ARCADEDRIVELEFTJOY_H
 #define ARCADEDRIVELEFTJOY_H
 
+int cube_drive_l_leftjoy(int drive_l_leftjoy);				  // arcade drive cube scale factor left joystick control
+int cube_drive_r_leftjoyl(int drive_r_leftjoy);				  // arcade drive cube scale factor left joystick control
+
 
 #endif
