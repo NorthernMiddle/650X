995819f215243a23b41537701afef183bf2eef38
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-16 21:56:19 -0500

    Update arcadeDriveRightJoy.cpp

--- sources/arcadeDriveRightJoy.cpp
+++ sources/arcadeDriveRightJoy.cpp
@@ -1,9 +1,9 @@
 /*----------------------------------------------------------------------------------------*/
 /*                                                                                        */
-/*   	Module:       arcadeDriveLeftJoy.cpp                                                */
+/*   	Module:       arcadeDriveRightJoy.cpp                                               */
 /*   	Author:       NMS_RP                                                                */
 /*   	Created:      16 Dec 2019                                                           */
-/*  	Description:  arcade drive left joystick with deadband and cubic                    */
+/*  	Description:   arcade drive right joystick with deadband and cubic                   */
 /*                   power input for VEX V5                                               */
 /*                                                                                        */
 /*----------------------------------------------------------------------------------------*/
@@ -12,41 +12,26 @@
 const int deadband = 15; 
 
 /** arcade drive variable intializations
-  * left joystick variable intializations
-  * - deadband variables intializations
-  * - drive variables intializations
-  *
   * right joystick variable intializations
   * - deadband variables intializations
   * - drive variables intializations
   */
-/* --- LEFT JOYSTICK --- */
-int forward_pct_leftjoy = Controller1.Axis3.position();
-int turn_pct_leftjoy = Controller1.Axis4.position();
-double drive_l_leftjoy = forward_pct_leftjoy + turn_pct_leftjoy;   // left '+'
-double drive_r_leftjoy = forward_pct_leftjoy - turn_pct_leftjoy;   // right '-'
 
 /* --- RIGHT JOYSTICK --- */
 int forward_pct_rightjoy = Controller1.Axis2.position();
 int turn_pct_rightjoy = Controller1.Axis1.position();
-double drive_l_rightjoy = forward_pct_rightjoy + turn_pct_rightjoy;   // left '+'
-double drive_r_rightjoy = forward_pct_rightjoy - turn_pct_rightjoy;   // right '-'
+double drive_l_rightjoy = Controller1.Axis2.value() + Controller1.Axis1.value();   // left '+'
+double drive_r_rightjoy = Controller1.Axis2.value() - Controller1.Axis1.value();   // right '-'
 
 
 /** Cubic Scale Factor for Arcade Drive Power
   * Functions used to achieve more sensitivity at low motor power
   * used for precise movements while still being able to use 100% power
   */
-int cube_drive_l_leftjoy(int drive_l_leftjoy){				// arcade drive cube scale factor left joystick control
-    return  pow(drive_l_leftjoy/100.0, 3.0)*100.0;
-}
-int cube_drive_r_leftjoyl(int drive_r_leftjoy){				// arcade drive cube scale factor left joystick control
-    return  pow(drive_r_leftjoy/100.0, 3.0)*100.0;
-}
 int cube_drive_l_rightjoy(int drive_l_rightjoy){			// arcade drive cube scale factor right joystick control
     return  pow(drive_l_rightjoy/100.0, 3.0)*100.0;
 }
-int cube_drive_r_rightjoyl(int drive_r_rightjoy){			// arcade drive cube scale factor right joystick control
+int cube_drive_r_rightjoy(int drive_r_rightjoy){			// arcade drive cube scale factor right joystick control
     return  pow(drive_r_rightjoy/100.0, 3.0)*100.0;
 
 ///////////////////////////////////////////////////////////////
@@ -64,20 +49,7 @@ int cube_drive_r_rightjoyl(int drive_r_rightjoy){			// arcade drive cube scale f
   */
 void arcadeDrive_leftJoy(){
   // DEADBAND, set to 0 if below the deadband threshold
-  if( abs( forward_pct_leftjoy ) < deadband ) forward_pct_leftjoy = 0;
-  if( abs( turn_pct_leftjoy    ) < deadband ) turn_pct_leftjoy = 0;
 
-  // send to motors
-  // --- left arcade motors ---
-  LFmotor.spin( forward, cube_drive_l_leftjoy, velocityUnits::pct );
-  LBmotor.spin( forward, cube_drive_l_leftjoy, velocityUnits::pct );
-  // --- right arcade motors ---
-  RFmotor.spin( forward, cube_drive_r_leftjoy, velocityUnits::pct );
-  RBmotor.spin( forward, cube_drive_r_leftjoy, velocityUnits::pct );
-
-  // allow other tasks to run
-  this_thread::sleep_for( 10 );
-}
 
 
 /**
