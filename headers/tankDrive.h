36579ae4856808cf704a1b94bac787e7f1610a9d
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-16 23:06:12 -0500

    Update tankDrive.h

--- headers/tankDrive.h
+++ headers/tankDrive.h
@@ -1,9 +1,9 @@
 #ifndef TANKDRIVE_H
 #define TANKDRIVE_H
 
-void deadBand_tankDrive();
+int deadBand_tankDrive();
 int cube_drive_lval(int drive_lval);    // tank drive cube scale factor
 int cube_drive_rval(int drive_rval);    // tank drive cube scale factor
-void tankDrive();
+int tankDrive();
 
 #endif
