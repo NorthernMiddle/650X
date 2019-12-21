49ef3733dc722137c98fdf1bac9e40a3ebbeb302
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-12-21 14:31:08 -0500

    Update trayLift.cpp

--- sources/trayLift.cpp
+++ sources/trayLift.cpp
@@ -1,44 +1,53 @@
-/*----------------------------------------------------------------------------------------*/
-/*                                                                                        */
-/*   	Module:       trayLift.cpp                                                        */
-/*   	Author:       NMS_RP                                                               */
-/*   	Created:      16 Dec 2019                                                          */
-/*  	Description:  Lift Func for Cube Tray - Tower Takeover           		*/
-/*                                                                                        */
-/*----------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       trayLift.cpp                                              */
+/*    Author:       NMS_RP                                                    */
+/*    Created:      Thur Oct 24 2019                                          */
+/*    Description:  Tray Lift Control                                         */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+// --- traylift.cpp ---
+#include "vex.h"
+using namespace vex;
 
 // traylift speed variable
 int trayLiftSpeedPCT = 100;       
 
 /** 	--- Tray Movement Shortend Function Call Setup ---
    *  tray lift up:     	cmd runs tray lift up manually by pressing of Button R1 (Manual Operation)
-   *  tray lift down:  	  	cmd runs tray lift down manually by pressing of Button R2 (Manual Operation)
+   *  tray lift down:  	  cmd runs tray lift down manually by pressing of Button R2 (Manual Operation)
    *  tray lift reset:  	cmd runs tray lift down from BUTTONDOWN press till limit switch is triggered (Automatic Reset)
    */
 void trayLiftSpinUp(){
-  TL.spin( reverse, trayLiftSpeedPCT, velocityUnits::pct );
+  TrayLiftmotor.spin( reverse, trayLiftSpeedPCT, velocityUnits::pct );
 }
 
 void trayLiftSpinDown(){
-  TLmotor.spin( forward, trayLiftSpeedPCT, velocityUnits::pct );
+  TrayLiftmotor.spin( forward, trayLiftSpeedPCT, velocityUnits::pct );
 }
 
 ///////////////////////////////////////////////////
 //
-// --- Tray Lift Function ---
+// --- Tray Lift Thread ---
 //
 ///////////////////////////////////////////////////
 
 // declare state variable for tray lift
 bool manual_mode = true;          
 
-void TLSelection(){
+int trayLift_f(){
+  int count = 0;
+  while(true){
+  Brain.Screen.setCursor(3,1);
+  Brain.Screen.print("Tray Lift has iterated %d times", count);
+  count++;
+
   // Manual: Tray Up by ButtonR1
   if((( manual_mode == true ) && (( Controller1.ButtonR1.pressing()) == true )))
   manual_mode = true;
   while(( manual_mode == true ) && (( Controller1.ButtonR1.pressing() == true))){
     trayLiftSpinUp();}
-    TLmotor.stop( brakeType::brake );
+    TrayLiftmotor.stop( brakeType::brake );
     manual_mode = true;
   
   // Manual: Tray Down by ButtonR2 with Hard Stop by Limit Switch   
@@ -46,7 +55,7 @@ void TLSelection(){
     manual_mode = true;
     while(( manual_mode == true ) && ( Controller1.ButtonR2.pressing() == true ) && 
     ( TrayLiftlimit.pressing() == false )){ trayLiftSpinDown(); }
-    TLmotor.stop( brakeType::brake );
+    TrayLiftmotor.stop( brakeType::brake );
     manual_mode = true;
   
   // Automatic: Tray Down Return by ButtonDown Press with Hard Stop by Limit Switch
@@ -54,9 +63,16 @@ void TLSelection(){
     manual_mode = false;
     while(( manual_mode == false ) && (( TrayLiftlimit.pressing()) == false )){
     trayLiftSpinDown(); }
-    TLmotor.stop( brakeType::brake );
+    TrayLiftmotor.stop( brakeType::brake );
     manual_mode = true;
 	
-/* Allow other tasks to run; don't hog the cpu :) */
-  vex::this_thread::sleep_for( 10 );
+	/* You must sleep threads by using the 'this_thread::sleep_for(unit in
+    msec)' command to prevent this thread from using all of the CPU's
+    resources. */
+  vex::this_thread::sleep_for( 25 );
+    
+  }
+  /* A threads's callback must return an int, even though the code will never
+  get here. You must return an int here. Threads can exit, but this one does not. */
+  return( 0 );
 }
