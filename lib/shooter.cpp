f9888af3037cfe666d2f11e006b7ec2dc1bfe710
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-10-20 05:50:43 -0400

    Create Flywheel File
    File for flywheel program

---
+++ lib/shooter.cpp
@@ -0,0 +1,10 @@
+/**
+ * shooter.cpp - some program to control shooter mechanism
+ *
+ * written for Northern Middle School Robotics
+ * 2018-19 VRC "Turning Point"
+ *
+ *
+ * @TODO:
+ * @TODO:
+ */
