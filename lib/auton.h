5e4deaeaad98dcde9693a80fab1f1049e1eb1978
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-10-23 16:00:40 -0400

    Create auton.h

---
+++ lib/auton.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       auton.h                                                   */
+/*    Author:       NMS_RP                                                    */
+/*    Created:      18 Dec 2019                                               */
+/*    Description:  autonomous program file holder header file                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#ifndef AUTON_H
+#define AUTON_H
+
+void Auton_Straight();
+
+void Auton_RL();
+void Auton_RR();
+
+void Auton_BL();
+void Auton_BR();
+
+void goAuton();
+
+#endif // AUTON_H
