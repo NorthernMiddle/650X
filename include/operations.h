850d8ea6b467ec4da56954f5c01692a701a81b17
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-10-25 13:11:35 -0400

    Update operations.h

--- include/operations.h
+++ include/operations.h
@@ -1,31 +1,37 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       operations.h                                              */
-/*    Author:       NMS_RP                                                    */
-/*    Date:         15 Jan 2020                                               */
-/*    Description:  header file for driver operations functions               */
-/*                    for Tower Takeover                                      */
+/*    Author:       LoJac10                                                   */
+/*    Date:         Oct 25th, 2022                                            */
+/*    Description:  header file for operations functions 650X "Xray"          */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+
+#ifndef INTAKE_H
+#define INTAKE_H
+
+  void intake_f();
+
+#endif
+
 
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
-// tank drive
-int deadBand_tankDrive();                         // deadband function for controller
-double ceil_drive_lval( double drive_lval );      // round function to 'whole unit' controller input
-double ceil_drive_rval( double drive_rval );      // round function to 'whole unit' controller input
-double cube_drive_lval( double ceildrive_lval );  // tank drive cube scale factor
-double cube_drive_rval( double ceildrive_rval );  // tank drive cube scale factor  
-int tankDrive_f();                                // tankdrive function call
+  // tank drive
+  int deadBand_tankDrive();                         // deadband function for controller
+  double ceil_drive_lval( double drive_lval );      // round function to 'whole unit' controller input
+  double ceil_drive_rval( double drive_rval );      // round function to 'whole unit' controller input
+  double cube_drive_lval( double ceildrive_lval );  // tank drive cube scale factor
+  double cube_drive_rval( double ceildrive_rval );  // tank drive cube scale factor  
+  int tankDrive_f();                                // tankdrive function call
 
-// intake (spinners)
-int intake_f();                                   // intake function call
+  // intake (spinners)
+  int intake_f();                                   // intake function call
 
-// tray lift
-void trayLiftSpinUp();                            // spin cubes up
-void trayLiftSpinDown();                          // spin cubes down     
-int trayLift_f();                                 // traylift function call
-
+  // tray lift
+  void trayLiftSpinUp();                            // spin cubes up
+  void trayLiftSpinDown();                          // spin cubes down     
+  int trayLift_f();                                 // traylift function call
 
 #endif // OPERATIONS_H
