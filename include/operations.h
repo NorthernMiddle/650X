04007d2607d316baac70d174dad010d4f6509e82
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-10-25 13:17:24 -0400

    Update operations.h

--- include/operations.h
+++ include/operations.h
@@ -9,6 +9,22 @@
 
 /////////////////////////
 //
+// arcadeDriveRightJoy.h
+//
+//////////////////////////////////
+#ifndef ARCADEDRIVERIGHTJOY_H
+#define ARCADEDRIVERIGHTJOY_H
+
+  int cube_drive_l_rightjoy(int drive_l_rightjoy);			 // arcade drive cube scale factor right joystick control
+  int cube_drive_r_rightjoyl(int drive_r_rightjoy);			// arcade drive cube scale factor right joystick control
+  void deadBand_arcadeRightJoy();
+  void arcadeDrive_rightJoy();
+
+#endif
+
+
+/////////////////////////
+//
 // flywheel.h
 //
 //////////////////////////////////
