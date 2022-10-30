c9fb78891051a947fba7653ddfaf13fced35f426
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-10-30 07:40:44 -0400

    Update auton.h

--- include/auton.h
+++ include/auton.h
@@ -1,45 +1,28 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
-/*    Module:       autonSelect.h                                             */
-/*    Author:       NMS_RP modified from jpearman's autonSelect.h             */
-/*    Created:      18 Dec 2019                                               */
-/*    Description:  autonomous selection program for V5                       */
+/*    Module:       auton.h                                                   */
+/*    Author:       LoJac10                                                   */
+/*    Created:      Oct 30th, 2022                                            */
+/*    Description:  auton program select module and auton                     */
+/*    program header file                                                     */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-#ifndef AUTONSELECT_H
-#define AUTONSELECT_H
 
+  // module: autonSelect.h
+  // author: jpearman's autonSelect.h
   typedef struct _button button;
   void displayButtonControls( int index, bool pressed );
   int findButton(  int16_t xpos, int16_t ypos );
   void initButtons();
   void userTouchCallbackPressed();
   void displayButtonControls( int index, bool pressed );
-  void goAuton();
 
-#endif // AUTONSELECT_H
-
-
-
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       auton.h                                                   */
-/*    Author:       NMS_RP                                                    */
-/*    Created:      18 Dec 2019                                               */
-/*    Description:  autonomous program file holder header file                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-#ifndef AUTON_H
-#define AUTON_H
-
-  void Auton_Straight();
-
-  void Auton_RL();
-  void Auton_RR();
-
-  void Auton_BL();
-  void Auton_BR();
-
-  void goAuton();
-
-#endif // AUTON_H
+  // module: auton programs
+  void 
+  void
+  void
+  void
+  void 
+  void
+  void
+  void
