136dcfbe1f76a7ea19bba3f976a97c0f11502c3a
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-10-30 07:10:37 -0400

    Update skills.cpp

--- routines/skills.cpp
+++ routines/skills.cpp
@@ -7,6 +7,12 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
+///////////////////////////////////////////
+//
+//	--- Skills Routine Selector ---
+//
+///////////////////////////////////////////
+
 /**	skillsRed.cpp
   *	
   *
@@ -23,25 +29,3 @@ void skillsBlue() {
   // enter your Blue Alliance skills program here...
 }
 
-///////////////////////////////////////////
-//
-//	--- Skills Routine Selector ---
-//
-///////////////////////////////////////////
-/*
-std::string skillsRoutines[] = { "Red, Blue" };
-void goSkills(int routine) {
-    switch(routine) {
-      case 0:
-			  // Red Skills Routine Selected
-			  Brain.Screen.printAt( 60, 125, "Red Skills Routine Selected ");
-        skillsRed();
-        break;
-      case 1:
-        // Blue Skills Routine Selected
-        Brain.Screen.printAt( 60, 125, "Blue Skills Routine Selected ");
-        skillsBlue();
-        break;
-    }
-}
-*/
