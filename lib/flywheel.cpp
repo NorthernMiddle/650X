1c9fc62d5db6ac6483dc9b6d043af1a67af809cc
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-11-01 09:15:21 -0400

    Update flywheel.cpp

--- lib/flywheel.cpp
+++ lib/flywheel.cpp
@@ -1,15 +1,18 @@
 motor_group Flywheel = motor_group(Fly1, Fly2);
 bool maintainSpeed = false;
 double targetspeed = 600;
-double kp = 0.5;
-double ki = 0.10;
-double kd = 0.05;
+
+double kp = 0.5;    // error rate of proportion
+double ki = 0.10;   // error rate of integral
+double kd = 0.05;   // error rate of derivative
+
 double preverror = 0.0;
 double error = 0.0;
 double totalError = 0.0; // += error
 double derivative = 0.0; // = error-preverror
 double flyspeed;
 double Power = 0;
+
 bool ReadyShoot = false;
 int FlyPID(){
   while(maintainSpeed){
@@ -21,12 +24,14 @@ int FlyPID(){
     else{
       ReadyShoot = false;
     }
+    
     Power = (error*kp + totalError * ki + (error - preverror) * kd)/12;
     Flywheel.spin(forward, Power, volt);
+    
     preverror = error;
     totalError += error;
+    
     vex::task::sleep(20);
-
   }
-  return 1;
+  return true;
 }
