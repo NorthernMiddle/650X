88f230105b75ef3ccb807165bf95c2fd61b5c1d4
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-11-01 09:05:20 -0400

    Create flywheel.cpp

---
+++ lib/flywheel.cpp
@@ -0,0 +1,32 @@
+motor_group Flywheel = motor_group(Fly1, Fly2);
+bool maintainSpeed = false;
+double targetspeed = 600;
+double kp = 0.5;
+double ki = 0.10;
+double kd = 0.05;
+double preverror = 0.0;
+double error = 0.0;
+double totalError = 0.0; // += error
+double derivative = 0.0; // = error-preverror
+double flyspeed;
+double Power = 0;
+bool ReadyShoot = false;
+int FlyPID(){
+  while(maintainSpeed){
+    flyspeed = (Fly1.velocity(rpm) + Fly2.velocity(rpm))/2; 
+    error = targetspeed - flyspeed;
+    if (error <= 0.1){
+      ReadyShoot = true;
+    }
+    else{
+      ReadyShoot = false;
+    }
+    Power += (error*kp + totalError * ki + (error - preverror) * kd)/12;
+    Flywheel.spin(forward, Power, volt);
+    preverror = error;
+    totalError += error;
+    vex::task::sleep(20);
+
+  }
+  return 1;
+}
