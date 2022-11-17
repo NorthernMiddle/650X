4ce6338c968757ebf1e6d698b8f746e897a8f892
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-11-17 12:26:41 -0500

    update main.cpp to howl
    add howl program to auton selection template

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,59 @@ using namespace vex;
 // A global instance of competition
 competition Competition;
 
+///
+// Set Motor Functions
+//  - this sets motors between -12000 and 12000.  i'm used to
+//  - -100 to 100, so the "scale" variable lets me use that as
+//  - inputs and scales it to -12000 to 12000
+///
+
+// Set voltage
+const int SCALE = 120;
+
+
+// set drive voltage
+void set_drive_pow(int l, int r) {
+  LeftFront.spin(fwd, l * SCALE, voltageUnits::mV);
+  LeftBack.spin(fwd, l * SCALE, voltageUnits::mV);
+  RightFront.spin(fwd, r * SCALE, voltageUnits::mV);
+  RightBack.spin(fwd, r * SCALE, voltageUnits::mV);
+}
+
+// set tank drive control
+void tank_drive(){
+  int l_joy = abs(Controller1.Axis3.value()) > 5 ? Controller1.Axis3.value() : 0;
+  int r_joy = abs(Controller1.Axis2.value()) > 5 ? Controller1.Axis2.value() : 0;
+  set_drive_pow(l_joy, r_joy);
+}
+
+// set arcade drive control
+void right_arcade_drive(){
+  // fwd Axis2 + Axis1
+  int l_joy = abs(Controller1.Axis2.value() + Controller1.Axis1.value()) > 5 ? Controller1.Axis2.value() + Controller1.Axis1.value() : 0;
+  // turn Axis2 - Axis1
+  int r_joy = abs(Controller1.Axis2.value() - Controller1.Axis1.value()) > 5 ? Controller1.Axis2.value() - Controller1.Axis1.value() : 0;
+  set_drive_pow(l_joy, r_joy);
+}
+
+// set intake voltage
+void set_intake(int input) {
+  Intake.spin(fwd, input * SCALE, voltageUnits::mV);
+}
+
+// set indexer voltage
+void set_indexer(int pos, int speed) {
+  Indexer.startRotateTo(pos, rotationUnits::deg, speed, velocityUnits::pct);
+}
+
+// set flywheel voltage
+void set_flywheel(int input) {
+  Flywheel1.spin(fwd, input * SCALE, voltageUnits::mV);
+  Flywheel2.spin(fwd, input * SCALE, voltageUnits::mV);
+}
+
+
+
 int autonomousSelection = -1;
 
 // collect data for on screen button
@@ -284,29 +337,86 @@ void autonomous( void ) {
 //  You must modify the code to add your own robot specific commands here. 
 //
 //////////////////////////////////////////////////////////////////////////////////////
+int speeds[] = {0, 127, 115, 110, 90};
+int speed = 0;
+bool indexer_pressed = false;
+int target = 0;
+int indexer_timer = 0;
 
 void usercontrol(void) {
   Brain.Screen.clearScreen();
     
   int count = 0;
+  bool last_r2 = false;
+    
+  // User control code here, inside the loop
   while ( true ) {
     Brain.Screen.printAt( 60,  125, "Driver Running %5d", count++ );
     
-    // thread call name indicates drive control 
-    thread tankdrive_t = thread(tankDrive_f);     // thread call to DRIVE FUNCTION
-      
-    // thread call name indicates method of intake -- game specific  
-    thread spinners_t = thread(intake_f);         // thread call to INTAKE FUNCTION
-      
-    // thread call name indicates mechanism #1 for game specific usages
-    thread lift_t = thread(trayLift_f);           // thread TRAY LIFT
-    // ...
-    // ...
-    // thread call name indicates mechanism #... for game specific usages
-    // thread xxx_t = thread(xxxx_f);
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    // ........................................................................
+    /* DRIVE CONTROL SELECTION
+       choose drive control here. Uncomment the call to the desired drive 
+       control */
     
-    // sleep threads. Allow other tasks to run
-    task::sleep(100); 
+    // tank drive
+    // tank_drive();
+      
+    // right arcade drive
+    right_arcade_drive();
+
+    // ..........................................................................
+    
+
+    // intake 
+    if (Controller1.ButtonL1.pressing()) {
+      set_intake(127); 
+    } else if (Controller1.ButtonL2.pressing()) {
+      set_intake(-127);
+    } else {
+      set_intake(0);
+    }
+
+    // flywheel
+    if (Controller1.ButtonR2.pressing() and !last_r2) {
+      if (speed >= 4) {
+        speed = 0;
+      } else {
+        speed++;
+      } 
+      set_flywheel(speeds[speed]);
+    }
+    printf("\n %d", speeds[speed]);
+    last_r2 = Controller1.ButtonR2.pressing();
+
+
+    // indexer
+    if (Controller1.ButtonR1.pressing()) {
+
+      indexer_pressed = true;
+    }
+    if (indexer_pressed) {
+      if (indexer_timer <= 17) {
+        target = 160;
+        indexer_timer++;
+      } else {
+        indexer_timer = 0;
+        target = 0;
+        indexer_pressed = false;
+      }
+    }
+    
+    set_indexer(target, 200);
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
   }
 }
 
