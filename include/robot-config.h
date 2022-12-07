edba0693dd3704842ca966a1061df395eb134b5a
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-12-07 17:24:37 -0500

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,36 +1,38 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       robot_config.h                                            */
-/*    Author:       LoJac10                                                   */
-/*    Created:      Oct 30th, 2022                                            */
-/*    Description:  Default header for robot_configuration file for           */
-/*                   VEX Projects (aka HAL)                                   */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
 using namespace vex;
 
+// brain
 extern brain Brain;
 
-// VEXcode devices
+// controller
 extern controller Controller1;
-extern motor intake;
-extern motor LFmotor;
-extern motor LBmotor;
+
+// sensors
+extern inertial DrivetrainInertial;
+
+// motors
+extern motor LeftFront;
+extern motor LeftBack;
+extern motor RightFront;
+extern motor RightBack;
+extern motor Indexer;
+extern motor Intake;
+extern motor Flywheel1;
+extern motor Flywheel2;
+
+// motor groups
+extern motor_group Flywheel;
 extern motor_group LeftSide;
-extern motor RFmotor;
-extern motor RBmotor;
 extern motor_group RightSide;
-extern drivetrain Drivetrain;
-extern motor flyWheel1;
-extern motor flywheel2;
-extern motor_group flyWheels;
+
+// drivetrain
+extern smartdrive robotDrive;
+
 
 
 /**
- * Used to initialize code/tasks/devices added using tools in VEXcode Text.
- *
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void);
+void  vexcodeInit( void );
 
