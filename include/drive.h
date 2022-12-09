5b19c1c8defc453c54e18eef015ba406917ea6ce
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-12-09 17:07:18 -0500

    Create drive.h

---
+++ include/drive.h
@@ -0,0 +1,12 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       drive.h                                                   */
+/*    Author:       LoJac10                                                   */
+/*    Date:         30 Nov 2022                                               */
+/*    Description:  header file for drive functions                           */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+void set_drive_pow(int l, int r);
+void right_arcade_drive();
+int arcadeDrive_f();
