953430b1dada9e4e74055d61713b8eb40dc6d7b5
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-12-09 17:12:22 -0500

    Update drive.cpp

--- lib/drive.cpp
+++ lib/drive.cpp
@@ -2,119 +2,79 @@
 /*                                                                            */
 /*    Module:       drive.cpp                                                 */
 /*    Author:       NMS_RP                                                    */
-/*    Created:      01 Oct 2022                                               */
+/*    Created:      28 Nov 2022                                               */
 /*    Description:  Operation control file for all driver functions           */
 /*                  and thread calls                                          */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-// VEX V5 C++ Project
 #include "vex.h"
-
 using namespace vex;
 
-// tank drive variables
-double l_joyStickValue, r_joyStickValue, ceil_JoyStickInputValue,
-       ceil_l_joyStickValue, ceil_r_joyStickValue, 
-       smoothDrive_lval, smoothDrive_rval, result01, result02,
-       velocityCurveSetting;
+//////////////////////////////////////////////////
+//
+// Set Motor Functions
+//
+//  - this sets motors between -12000 and 12000.  i'm used to
+//  - -100 to 100, so the "scale" variable lets me use that as
+//  - inputs and scales it to -12000 to 12000
+//
+////////////////////////////////////////////////////////////////////////////////
 
-/** --- Power Scale Factor Functions for Velocity Adjustment --- 
-  * Description: 
-  *    Functions used to achieve more sensitivity at low motor velocity
-  *    used for precise movements while still being able to use 100% velocity
-  @ ceiling functions
-  @ function defintions
-  @ calls for left & right joysticks
-  */
+// Set voltage
+const int SCALE = 120;
+const int DEADBAND = 15;
 
-/**
-  * Motor Velocity Acceleration Smoothing Function
-  *
-  * Ref: See _Colossus VEX Forum Sept. 2019
-  * Ref: 'What do you think is a more efficient way to drive your robot?'
-  * Ref: Post 41/47
-  *
-  @ PARAM base value --> joystick input value
-  @ PARAM exponent value --> expValue selected smoothing velocity curve
-  */ 
-double motorVelocitySmoothValues(double ceil_l_joyStickValue, double ceil_r_joyStickValue, int expValue){
-  smoothDrive_lval = ( (pow(ceil_l_joyStickValue, expValue)) / (pow( 100, (expValue - 1))) );
-  smoothDrive_rval = ( (pow(ceil_r_joyStickValue, expValue)) / (pow( 100, (expValue - 1))) );
-  return 0;
+
+// set drive voltage
+void set_drive_pow(int l, int r) {
+  LeftFront.spin(fwd, l * SCALE, voltageUnits::mV);
+  LeftBack.spin(fwd, l * SCALE, voltageUnits::mV);
+  RightFront.spin(fwd, r * SCALE, voltageUnits::mV);
+  RightBack.spin(fwd, r * SCALE, voltageUnits::mV);
 }
 
-/** --- tankDrive_f is a callback function ---
+/*
+// set tank drive control
+void tank_drive(){
+  int l_joy = abs(Controller1.Axis3.value()) > DEADBAND ? Controller1.Axis3.value() : 0;
+  int r_joy = abs(Controller1.Axis2.value()) > DEADBAND ? Controller1.Axis2.value() : 0;
+  set_drive_pow(l_joy, r_joy);
+}
+*/
+
+// set arcade drive control
+void right_arcade_drive(){
+  // fwd Axis2 + Axis1
+  int l_joy = abs(Controller1.Axis2.value() + Controller1.Axis1.value()) > DEADBAND ? Controller1.Axis2.value() + Controller1.Axis1.value() : 0;
+  // turn Axis2 - Axis1
+  int r_joy = abs(Controller1.Axis2.value() - Controller1.Axis1.value()) > DEADBAND ? Controller1.Axis2.value() - Controller1.Axis1.value() : 0;
+  set_drive_pow(l_joy, r_joy);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/** --- arcadeDrive_f is a callback function ---
   * Description:
-  *   Function for tank drive. Function to be registered to a thread for continuous use.
-  *   Function registered to thread tankdrive_t
+  *   Function for arcade drive for right joystick. Function to be registered to a thread for continuous use.
+  *   Function registered to thread arcadeDrive_t
   */
-int tankDrive_f(){
-
-  const int Deadband = 5; 
-  int count, velocityCurveSetting;
-
-  count = 0;
-  while(true){
+int arcadeDrive_f(){
+  int count = 0;
+  while(true)
+  {
     Brain.Screen.setCursor(1,1);
-    Brain.Screen.print("TankDrive has iterated %d times", count);
+    Brain.Screen.print("Arcade Drive has iterated %d times", count);
     count++;
 
-    // tank drive variable declare & intialize
-    l_joyStickValue = Controller1.Axis3.position();   // left-joystick tank drive control
-    r_joyStickValue = Controller1.Axis2.position();   // right-joystick tank drive control
 
-    // display velocity values
-    Brain.Screen.setCursor(3,1);
-    Brain.Screen.print("l_joystick: %3.2f ", l_joyStickValue);
-    Brain.Screen.setCursor(4,1);
-    Brain.Screen.print("r_joystick: %3.2f ", r_joyStickValue);
-
-    // deadband, set to 0 if below the deadband value
-    // fabs --> floating absolute value -- C++11
-    if( fabs( l_joyStickValue ) < Deadband ) l_joyStickValue = 0;
-    if( fabs( r_joyStickValue ) < Deadband ) r_joyStickValue = 0;
-  
-    // ceiling up joystick input values
-    ceil_l_joyStickValue = ceil(l_joyStickValue);
-    ceil_r_joyStickValue = ceil(r_joyStickValue);
-
-    // display velocity values
-    Brain.Screen.setCursor(6,1);
-    Brain.Screen.print("ceil_l_joystick: %3.2f ", ceil_l_joyStickValue);
-    Brain.Screen.setCursor(7,1);
-    Brain.Screen.print("ceil_r_joystick: %3.2f ", ceil_r_joyStickValue);
- 
-    // acceleration smoothing function call and variable assignment
-    // acceleration curve is using a cubic function model
-    velocityCurveSetting = 3;
-    motorVelocitySmoothValues(ceil_l_joyStickValue, ceil_r_joyStickValue, velocityCurveSetting);
-    
-    // display velocity values
-    Brain.Screen.setCursor(9,1);
-    Brain.Screen.print("l_motors -->   %Raw: %3.2f   Fix: %3.2f", l_joyStickValue, smoothDrive_lval );
-    Brain.Screen.setCursor(10,1);
-    Brain.Screen.print("r_motors -->   %Raw: %3.2f   Fix: %3.2f", r_joyStickValue, smoothDrive_rval );
-
-    /* --- send velocity value to motors --- */
-    // left tank motors
-    LFmotor.spin( directionType::fwd, smoothDrive_lval, velocityUnits::pct );
-    LBmotor.spin( directionType::fwd, smoothDrive_lval, velocityUnits::pct );
-
-    // right tank motors
-    RFmotor.spin( directionType::fwd, smoothDrive_rval, velocityUnits::pct );
-    RBmotor.spin( directionType::fwd, smoothDrive_rval, velocityUnits::pct );
-
-
+    right_arcade_drive();
     /* You must sleep threads by using the 'this_thread::sleep_for(unit in
-      msec)' command to prevent this thread from using all of the CPU's
-      resources. */
+    msec)' command to prevent this thread from using all of the CPU's
+    resources. */
     this_thread::sleep_for( 25 );
   }
 
-/* A threads's callback must return an int, even though the code will never
+  /* A threads's callback must return an int, even though the code will never
   get here. You must return an int here. Threads can exit, but this one does not. */
-return 0;
+  return( 0 );
 }
-
-
