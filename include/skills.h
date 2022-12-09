4636264fa10606ff44c716229fd7f5ff89239611
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-12-09 17:05:38 -0500

    Create skills.h

---
+++ include/skills.h
@@ -0,0 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       skills.h                                                  */
+/*    Author:       LoJac10                                                   */
+/*    Date:         9 December 2022                                           */
+/*    Description:  header file for skills functions 650X "Xray"              */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// TODO: Add header files for skills run
