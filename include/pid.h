c86f727f46aa863a815dfa415521fb855610bd32
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2022-12-09 17:18:39 -0500

    Create pid.h

---
+++ include/pid.h
@@ -0,0 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       pid.cpp                                                   */
+/*    Author:       LoJac10                                                   */
+/*    Created:      Dec 9th, 2022                                             */
+/*    Description:  PID is used to accuractely move & turn some distance      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// left blank on purpose
